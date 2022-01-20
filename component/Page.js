5aba9832719cdc67d881e0adc66865e1b7b14b4a
Alvin Leung <alvinleung2009@gmail.com>
2022-01-19 18:33:24 -0800

    fix rendering bug

--- component/Page.js
+++ component/Page.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Page({ src, children }) {
   return (
     <div className="w-full relative bg-dark">
-      <div className="relative w-full ">
+      <div className="relative w-full">
         <img src={src} alt="" className="w-full block" />
         {children}
       </div>
